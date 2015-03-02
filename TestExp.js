f84e3133c1cdf26a7de4284e0d80f166bd32a373
m-Py <martin.papenberg@hhu.de>
2015-03-02 12:07:03 +0100

    played with test experiment

--- TestExp.js
+++ TestExp.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 			expArr[i] = fixationCross;
 		}
 		else {
-			expArr[i] = new Square("stim", "st"+i, rndSize(), 800, 1000, rndCol());
+			expArr[i] = new Square("stim", "st"+i, rndSize(), 800, 1000, rndCol()); // Parent div is always the same, but each new Square object has a seperate div which contains the stimulus
 		}
 	}
-	expArr.push(new Text("stim", "schluss", 50, 0, 0, "The experiment is over =)", rndCol())); // text is presented in the end. Duration = 0 -> not removed
+	expArr.push(new Text("stim", "end", 50, 0, 0, "The experiment is over")); // text is presented in the end. Duration = 0 -> not removed
 	
 	// start the experiment!
 	(startExp(expArr)); 
