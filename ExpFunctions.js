c5f146425d5a2378120f4dcc6940483f2a64146c
m-Py <martin.papenberg@hhu.de>
2015-03-03 10:34:47 +0100

    startExp now can handle fixed and variable durations of stimuli

--- ExpFunctions.js
+++ ExpFunctions.js
@@ -1,88 +1,92 @@
-		// create partial arrays. Presentation of stimuli must be stopped when the duration of 1 stimulus is 0.
-		var partExp = function(arr) {
-			var j = 0; // count the partial arrays that we need
-			var partArr = [[]];
-			for (var i = 0; i < arr.length; i++ ) {
-				if (arr[i].duration === 0) { 
-					partArr[j].push(arr[i]);
-					partArr.push([]);
-					j++;
-				}
-				else { 
-					partArr[j].push(arr[i]);
-				}
-			}
-			return partArr;
-		};
 
-		// runStimuli takes an array containing stimuli objects and runs all stimuli. This is the old startExp function 
-		// is still called by startExp; if my restructuring of startExp fails, use this function
-		var runStimuli = function(arr) {
-			// var that controls the timing of the experiment
-			var ExperimentalDelay = 0;
-			// present the stimuli
-			for (var i = 0; i < arr.length; i++ ) {
-				var j = 0;
-				setTimeout(function() { arr[j].present(arr[j].duration); j++}, ExperimentalDelay); // calls a new stimulus after duration and ISI of the previous stimulus
-				ExperimentalDelay = ExperimentalDelay + arr[i].duration + arr[i].ISI;
-			}
-			return(ExperimentalDelay) // returns how long the experiment runs
-		};
-		
-
-	// Function that runs the experiment. Calls the stimuli from an array one after another.
-	var startExp = function(arr) { 
-		var Stimuli = partExp(arr); // partition passed array
-		var ExperimentalDelay = 0;
-		for (var i = 0; i < Stimuli.length; i++ ) {
-			var j = 0;
-			// how long do the Stimuli run
-			var currentDelay = 0;
-			for (var t = 0; t < Stimuli[i].length; t++) {
-				currentDelay = currentDelay + Stimuli[i][t].duration + Stimuli[i][t].ISI;
-			}
-			setTimeout(function() {
-				$("body").append("<div id='startMe'></div>");
-				$("#startMe").css("height", $(window).height());
-				$("#startMe").css("width", $(window).width());
-				$("#startMe").click(function() {
-					$("#startMe").remove();
-					runStimuli(Stimuli[j]);
-					if (j > 0) {
-						var tmp = Stimuli[j-1].length;
-						$(Stimuli[j-1][tmp].dummyDiv).remove();
-					}
-					j++;
-					
-						
-				});
-			}, currentDelay)
-			
-			
+// create partial arrays. Presentation of stimuli must be stopped when the duration of 1 stimulus is 0.
+var partExp = function(arr) {
+	var j = 0; // count the partial arrays that we need
+	var partArr = [[]];
+	for (var i = 0; i < arr.length; i++ ) {
+		if (arr[i].duration === 0) { 
+			partArr[j].push(arr[i]);
+			partArr.push([]);
+			j++;
 		}
-	};
-
-
-	// function that removes a given div after a specified duration. Give duration argument in ms.
-	var countdown = function(duration, div) {
-		var timeLeft = duration/10;
-		var countdown = setInterval(function() {
-		timeLeft--; // countdown
-		if (timeLeft <= 0) {
-			clearInterval(countdown)
-			$(div).remove();
+		else { 
+			partArr[j].push(arr[i]);
 		}
-		}, 10); // timing precision of 10ms
+	}
+	return partArr;
+};
+
+
+// runStimuli actually presents the stimuli: takes an array containing stimuli objects as argument and presents all stimuli
+var runStimuli = function(partArr) {
+	// var that controls the timing of the experiment
+	var ExperimentalDelay = 0;
+	// present the stimuli
+	for (var i = 0; i < partArr.length; i++ ) {
+		var j = 0;
+		setTimeout(function() { partArr[j].present(partArr[j].duration); j++}, ExperimentalDelay); // calls a new stimulus after duration and ISI of the previous stimulus
+		ExperimentalDelay = ExperimentalDelay + partArr[i].duration + partArr[i].ISI;
+	}
+};
+
+	
+// function that computes the time that an experimental unit needs to finish
+var getExpTime = function(partArr) {
+	var currentDelay = 0;
+	// to consider: ignore ISI of last stimulus?
+	for (var t = 0; t < partArr.length; t++) {
+		currentDelay = currentDelay + partArr[t].duration + partArr[t].ISI;
+	}		
+	return currentDelay
+};			
+
+
+// Function that runs the experiment. TO RUN YOUR EXPERIMENT, CALL THIS FUNCTION AND PASS THE ARRAY THATS CONTAINS ALL STIMULI
+var startExp = function(arr) { 
+	var Stimuli = partExp(arr); // partition passed array into several units. Partition is at each stimulus that has a duration of 0!
+
+	var startTimer = function(arr, counter) {
+		setTimeout(function() { startOnClick(arr, counter); }, getExpTime(arr[counter-1])); // calls startsOnClick after the stimuli of the current set have been presented
 	};
 	
-	
-	// create a random rgb color: "rgb(x,y,z)"
-	var rndCol = function() {
-		return("rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")");
-	};	
-	
-	
-	// create a random number, max 350. Can be used in order to randomize the size of a stimulus
-	var rndSize = function() {
-		return(Math.floor(Math.random()*340)+10);
+	var startOnClick = function(arr, counter) {
+		// there must be a better way to get an event handler that only works in this situation
+		$("body").append("<div id='startMe'></div>");
+		$("#startMe").css("height", $(window).height());
+		$("#startMe").css("width", $(window).width());
+		$("#startMe").click(function() {
+			$("#startMe").remove();
+			if (counter > 0 && counter < arr.length-1) {
+				$(arr[counter-1][arr[counter-1].length-1].dummyDiv).remove(); // probably the most ugly code ever; but it does remove the most recently presented stimulus ;-)
+			}
+			runStimuli(arr[counter]);
+			startTimer(arr, counter+1); // call startOnClick again after a timer has run out: then, the next stimuli are called
+		});
 	};
+	startOnClick(Stimuli, 0); // start experiment with first experimental unit
+};
+
+
+// function that removes a given div after a specified duration. Give duration argument in ms.
+var countdown = function(duration, div) {
+	var timeLeft = duration/10;
+	var countdown = setInterval(function() {
+	timeLeft--; // countdown
+	if (timeLeft <= 0) {
+		clearInterval(countdown)
+		$(div).remove();
+	}
+	}, 10); // timing precision of 10ms
+};
+	
+	
+// create a random rgb color: "rgb(x,y,z)"
+var rndCol = function() {
+	return("rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")");
+};	
+	
+	
+// create a random number, max 350. Can be used in order to randomize the size of a stimulus
+var rndSize = function() {
+	return(Math.floor(Math.random()*340)+10);
+};
--- TestExp.js
+++ TestExp.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 			expArr.push(new Square("stim", "st"+i, rndSize(), 800, 1000, rndCol())); // Parent div is always the same, but each new Square object has a seperate div which contains the stimulus
 		}
 	}
-	expArr.push(new Text("stim", "end", 50, 0, 500, "The experiment is over")); 
+	expArr.push(new Text("stim", "end", 50, 0, 0, "The experiment is over")); 
 	expArr.push(new Text("stim", "bla", 50, 500, 500, "But wait...")); 
 	expArr.push(new Text("stim", "moe", 50, 500, 500, "I think...")); 
 	expArr.push(new Text("stim", "shu", 50, 500, 500, "I still got it!"));
@@ -26,10 +26,21 @@ $(document).ready(function() {
 			expArr.push(new Square("stim", "st"+i, rndSize(), 800, 1000, rndCol())); // Parent div is always the same, but each new Square object has a seperate div which contains the stimulus
 		}
 	}	
-	expArr.push(new Text("stim", "end", 50, 0, 500, "Now it's really over ;-)")); 
+	expArr.push(new Text("stim", "end", 50, 0, 0, "Now it's really over ;-)")); 
+	expArr.push(new Text("stim", "end", 50, 1000, 500, "Or is it.....")); 
+	for (var i = 0; i < 20; i++) {
+		if (i % 2 === 0) {
+			expArr.push(fixationCross);
+		}
+		else {
+			expArr.push(new Square("stim", "st"+i, rndSize(), 800, 1000, rndCol())); // Parent div is always the same, but each new Square object has a seperate div which contains the stimulus
+		}
+	}		
+	expArr.push(new Text("stim", "end", 50, 0, 500, "=)"));
 	
+	 	
 	// start the experiment!
-	startExp(expArr);
+	startExp((expArr));
 	console.log(partExp(expArr));
 	
 });
