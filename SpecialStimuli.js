cd0358ba5f174195ac3053f3fc1dc4afddf0ad06
m-Py <martin.papenberg@hhu.de>
2015-03-23 13:05:57 +0100

    removed Countdown text stimulus

--- SpecialStimuli.js
+++ SpecialStimuli.js
@@ -1,31 +1,3 @@
-	// Text stimulus. During presentation a countdown is shown; so use even seconds as durations 
-	function CountdownText(Parent, Name, size, duration, ISI, text, color, cdSize) {
-		Text.call(this, Parent, Name, size, duration, ISI, text, color); //  call constructor from super class
-		this.cdSize = cdSize;
-	}
-	CountdownText.prototype = Object.create(Text.prototype, {
-		contructor: {
-			configurable: true,
-			enumerable: true,
-			value: CountdownText,
-			writable: true
-		}
-	});
-	// adds countdown to text displayed
-	CountdownText.prototype.showStimulus = function() {
-		Text.prototype.showStimulus.call(this);			
-		$(this.dummyDiv).prepend("<div id='countdown'></div>");
-		$("#countdown").css("position", "relative");
-		$("#countdown").css("left", "-50%");
-		var height = $(window).height();
-		$("#countdown").css("top", (height-this.size*2)/2);
-		$("#countdown").css("font-size", this.cdSize);
-	};
-	CountdownText.prototype.present = function() {
-		Stimulus.prototype.present.call(this);
-	};	
-
-
 
 	// Stimulus Object that can consist of multiple stimuli
 	function MultiStim() {
