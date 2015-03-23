427f1c36c93f1bab67d56172d96d2bd73c0ff8f8
m-Py <martin.papenberg@hhu.de>
2015-03-23 11:46:16 +0100

    added MultiStim to special stimuli

--- SpecialStimuli.js
+++ SpecialStimuli.js
@@ -20,17 +20,44 @@
 		var height = $(window).height();
 		$("#countdown").css("top", (height-this.size*2)/2);
 		$("#countdown").css("font-size", this.cdSize);
-		// Anzeige des Countdowns
-		var timeLeft = (this.duration/1000)+1;
-		$("#countdown").html(--timeLeft);
-		var countdown = setInterval(function() {
-			$("#countdown").html(--timeLeft);
-			if (timeLeft <= 0) {
-				$("#countdown").css("color", "transparent");
-				clearInterval(countdown)
-			}
-		}, 1000);
 	};
 	CountdownText.prototype.present = function() {
 		Stimulus.prototype.present.call(this);
 	};	
+
+
+
+	// Stimulus Object that can consist of multiple stimuli
+	function MultiStim() {
+		var _longestDuration = 0;
+		var _longestISI = 0;
+		for (var i = 0; i < arguments.length; i++) {
+			this["stimulus"+i] = arguments[i];
+			if (arguments[i].duration >= _longestDuration) { _longestDuration = arguments[i].duration; }
+			if (arguments[i].ISI >= _longestISI) { _longestISI = arguments[i].ISI; }
+		}
+		this.duration = _longestDuration
+		this.ISI = _longestISI
+	}
+	MultiStim.prototype = Object.create(Stimulus.prototype, { 
+		contructor: {
+			configurable: true,
+			enumerable: true,
+			value: MultiStim,
+			writable: true
+		}
+	});
+	MultiStim.prototype.showStimulus = function() {
+		for (var property in this) {
+			if (property.indexOf("stimulus") > -1) { // call stimuli that are bound in this object
+				this[property].showStimulus();
+			}
+		}
+	};
+	MultiStim.prototype.present = function() {
+		for (var property in this) {
+			if (property.indexOf("stimulus") > -1) { // call stimuli that are bound in this object
+				this[property].present();
+			}
+		}
+	};
