343490996b78f5fb07fc896c6a5e10e71f44715d
m-Py <martin.papenberg@hhu.de>
2015-03-25 17:14:25 +0100

    added pseudo code for implementation of experiment object

---
+++ expObject.js
@@ -0,0 +1,13 @@
+// Store the experiment in a JavaScript object; experiment gets started by method start, which calls startExp()
+
+
+var Experiment = {
+	container: "body", // either body or specified id of a div
+	start: startExp, // Method: call to start experiment
+	printResults: undefined, // Method: print results of the experiment; especially correctness and RT of responsescreate function for this
+	contains: 0, // Method: returns how many Stimuli are contained in the experiment. Gets increased by add()
+	add: undefined, // Method: Adds 1 Stimulus to the experiment. 
+	addBlock: undefined, // Method: Adds a block of Stimuli to the experiment. Pass a block of stimuli and a repetition number
+	expArr: [], // Array: contains all stimuli of the experiment; add and addBlock push Stimuli to this array
+};	
+	
