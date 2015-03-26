e515dbbb49de79e273c5b20a1aa027e687b7f0c1
m-Py <martin.papenberg@hhu.de>
2015-03-26 13:06:18 +0100

    modified test experiment

--- TestExp.js
+++ TestExp.js
@@ -5,22 +5,24 @@ $(document).ready(function() {
 	
 	myExp = new Experiment("stim");
 	
-	var stim1 = new Stimulus(1000, 400, true);
-	stim1.addFeature("Hello World", "text");
+	var stim1 = new Stimulus(1000, 400, false);
+	stim1.addText("Hello World", 100);
 	
-	var stim2 = new Stimulus(1400, 400, true);
-	stim2.addFeature("Moep", "text");	
+	var stim2 = new Stimulus(1000, 500, false);
+	stim2.addText("Moep", 100);	
 
-	var stim3 = new Stimulus(1400, 400, true);
-	stim2.addFeature("", "text");		
+	var stim3 = new Stimulus(1000, 500, false);
+	stim3.addText("Shanol");		
+
+	var stim4 = new Stimulus(1000, 500, false);
+	stim4.addText("FU U");			
 	
 	
 	myExp.add(stim1);
-	myExp.add(stim3);
 	myExp.add(stim2);
-	
-	
-			
+	myExp.add(stim3);
+	myExp.add(stim4);	
+				
 	myExp.start();
 	
 
