a4c2c6d8945070036ee51791e9845be874d90d51
m-Py <martin.papenberg@hhu.de>
2015-03-31 13:31:07 +0200

    added more support for response storage

--- TestExp.js
+++ TestExp.js
@@ -11,20 +11,23 @@
 	var stim0 = new Stimulus(0, 0, false);
 	stim0.addText("Start", 100, rndCol());
 	
-	var stim1 = new Stimulus(1000, 500, true, allowedKeys);
-	stim1.addText("Hello World", 100, rndCol());
+	var stim1 = new Stimulus(1000, 500, true, allowedKeys, 115);
+	stim1.addText("left", 100, rndCol());
 	
-	var stim2 = new Stimulus(1000, 500, true, allowedKeys);
-	stim2.addText("Moep", 100, rndCol());	 
+	var stim2 = new Stimulus(1000, 500, true, allowedKeys, 108);	
+	stim2.addText("right", 100, rndCol());	 
 
-	var stim3 = new Stimulus(1000, 500, true, allowedKeys);
-	stim3.addText("Shanol", 100, rndCol());		
+	var stim2_1 = new Stimulus(1000, 500, true, allowedKeys, "nogo");
+	stim2_1.addText("nothing", 50, rndCol());	
 
-	var stim4 = new Stimulus(1000, 500, true, allowedKeys);
-	stim4.addText("Schaf", 100, rndCol());			
+	var stim3 = new Stimulus(1000, 500, true, allowedKeys, 115);
+	stim3.addText("left", 100, rndCol());		
+
+	var stim4 = new Stimulus(1000, 500, true, allowedKeys, 108);
+	stim4.addText("right", 100, rndCol());			
 	
-	var stim5 = new Stimulus(1000, 500, true); // has no specified allowed keys; will record any given response
-	stim5.addText("Tralala", 50, rndCol());	
+	var stim5 = new Stimulus(1000, 500, true, allowedKeys, "nogo");
+	stim5.addText("nothing", 50, rndCol());	
 	
 	var stim6 = new Stimulus(0, 0, false);
 	stim6.addText("The experiment is over.", 50, rndCol());	
@@ -36,6 +39,8 @@
 	myExp.add(cross);
 	myExp.add(stim2);
 	myExp.add(cross);
+	myExp.add(stim2_1);
+	myExp.add(cross);	
 	myExp.add(stim3);
 	myExp.add(cross);
 	myExp.add(stim4);	
