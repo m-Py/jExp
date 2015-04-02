4e7269f20b434437e07adfb8c9bb9692cc9a140c
m-Py <martin.papenberg@hhu.de>
2015-04-02 09:58:58 +0200

    moved code that specifies behavior of stimuli to Stimulus_util.js

--- Stimulus.js
+++ Stimulus.js
@@ -15,7 +15,6 @@ function Stimulus(id, duration, ISI, listenTo, correctResponse) {
 	this.correct; // written to by listen(), which is called by present.
 	this.event; //  written to by listen(), which is called by present.
 	
-	this.featureNumber = 0;
 	this.features = []; // features of the stimulus that will be called by showStimulus()
 	this.experiment; // points to the experiment, which calls the Stimulus. This property is added to the stimulus, when it is added to an Experiment via .add() or .addBlock()
 }
@@ -105,83 +104,5 @@ Stimulus.prototype.present = function() {
 	// 3) remove stimulus after its specified duration
 	if (this.duration !== 0) {
 		this.experiment.countdown(this.duration);
-	}
-};
-
-
-// prototype methods to add features to Stimuli
-// add text
-Stimulus.prototype.addText = function(text, size, color, x1, y1) { // name feature type and coordinates, radius, size etc. Overloading is necessary here; see how to best implement it
-	var that = this;
-	that.text = text;
-	var draw = function () {
-		that.experiment.context.font = ""+size + "px Arial" || "30px Arial";
-		that.experiment.context.fillStyle = color || "black";
-		that.experiment.context.textAlign = "center";
-		that.experiment.context.fillText(that.text, that.experiment.canvas.width/2, (that.experiment.canvas.height/2)+(size/2.5)); // trying to vertically align text
-	};
-	that.features[that.featureNumber] = draw;
-	that.featureNumber = that.featureNumber + 1;
-};
-// add fixation cross
-Stimulus.prototype.addCross = function(size, width) {
-	var that = this;
-	var draw = function() {
-		that.experiment.context.beginPath();
-		that.experiment.context.lineWidth = width;
-		// horizontal line
-		that.experiment.context.moveTo((that.experiment.canvas.width/2) + size/2, (that.experiment.canvas.height/2));
-		that.experiment.context.lineTo((that.experiment.canvas.width/2) - size/2, (that.experiment.canvas.height/2));
-		// vertical line
-		that.experiment.context.moveTo((that.experiment.canvas.width/2), (that.experiment.canvas.height/2) + size/2);
-		that.experiment.context.lineTo((that.experiment.canvas.width/2), (that.experiment.canvas.height/2) - size/2);
-		that.experiment.context.stroke();
-	};
-	that.features[that.featureNumber] = draw;
-	that.featureNumber = that.featureNumber + 1;	
-};
-// use the prototype addCode function to execute code during runtime of the experiment
-Stimulus.prototype.addCode = function(code) {
-	var draw = function() { eval(code); };
-	this.features[this.featureNumber] = draw;
-	this.featureNumber = this.featureNumber + 1;	
-};
-// store specified results in an object
-Stimulus.prototype.addLogger = function(toBeSavedStimuli) {
-	
-	saveMe = arguments;
-	var that = this;
-	
-	var save = function() {
-		var saveData = ["id", "repetition", "RT", "event", "correct"];
-		
-		for (var i = 0; i < saveMe.length; i++) {
-			var data = {};
-			for (key in saveMe[i]) {
-				if ( $.inArray(key, saveData) !== -1 ) { 
-					data[key] = saveMe[i][key];
-				}
-			}
-			that.experiment.data.push(data);
-		}
-	};
-	this.features[this.featureNumber] = save;
-	this.featureNumber = this.featureNumber + 1;
-};
-
-
-Stimulus.prototype.removeFeatures = function() { // add functionality to replace the content of a stimulus by other content; remove all features!
-	var that = this;
-	that.featureNumber = 0;
-	that.features = [];
-};
-
-Stimulus.prototype.toString = function() {
-	
-	if (this.event) {
-		return("id: " + this.id + ",\n RT: " + this.RT + ",\n event: " + this.event + ",\n correct: " + this.correct);
-	}
-	else {
-		return("id: " + this.id);
 	}
 };
---
+++ Stimulus_util.js
@@ -0,0 +1,85 @@
+/* These are methods for the Stimulus object that define its functioning.
+   By itself, a stimulus does not do anything. This file contains methods 
+   that define the appearance of a stimulus, or add experimental utility.
+   * For example the addCode() method can be used to execute JavaScript Code
+   at a specific time during the experiment.
+   * Feature adding methods start with add and should contain be self describing.
+   * add methods must contain a function containing code to be executed when the 
+   stimulus is presented. This function must get pushed to the features array property
+   of the Stimulus.
+*/
+
+// add text
+Stimulus.prototype.addText = function(text, size, color, x1, y1) { // name feature type and coordinates, radius, size etc. Overloading is necessary here; see how to best implement it
+	var that = this;
+	that.text = text;
+	var draw = function () {
+		that.experiment.context.font = ""+size + "px Arial" || "30px Arial";
+		that.experiment.context.fillStyle = color || "black";
+		that.experiment.context.textAlign = "center";
+		that.experiment.context.fillText(that.text, that.experiment.canvas.width/2, (that.experiment.canvas.height/2)+(size/2.5)); // trying to vertically align text
+	};
+	that.features.push(draw);
+};
+// add fixation cross
+Stimulus.prototype.addCross = function(size, width) {
+	var that = this;
+	var draw = function() {
+		that.experiment.context.beginPath();
+		that.experiment.context.lineWidth = width;
+		// horizontal line
+		that.experiment.context.moveTo((that.experiment.canvas.width/2) + size/2, (that.experiment.canvas.height/2));
+		that.experiment.context.lineTo((that.experiment.canvas.width/2) - size/2, (that.experiment.canvas.height/2));
+		// vertical line
+		that.experiment.context.moveTo((that.experiment.canvas.width/2), (that.experiment.canvas.height/2) + size/2);
+		that.experiment.context.lineTo((that.experiment.canvas.width/2), (that.experiment.canvas.height/2) - size/2);
+		that.experiment.context.stroke();
+	};
+	that.features.push(draw);	
+};
+// use the prototype addCode function to execute code during runtime of the experiment
+Stimulus.prototype.addCode = function(code) {
+	var draw = function() { 
+		eval(code); 
+	};
+	that.features.push(draw);
+};
+// store specified results in an object
+// arguments must be of type Stimulus
+Stimulus.prototype.addLogger = function(toBeSavedStimuli) {
+	
+	saveMe = arguments;
+	var that = this;
+	
+	var save = function() {
+		var saveData = ["id", "repetition", "RT", "event", "correct"];
+		
+		for (var i = 0; i < saveMe.length; i++) {
+			var data = {};
+			for (key in saveMe[i]) {
+				if ( $.inArray(key, saveData) !== -1 ) { 
+					data[key] = saveMe[i][key];
+				}
+			}
+			that.experiment.data.push(data);
+		}
+	};
+	that.features.push(save);
+};
+
+// remove all features from a Stimulus.
+Stimulus.prototype.removeFeatures = function() { 
+	this.features = [];
+};
+
+// deprecated toString method. To show data, rather use Experiment.saveResults and log it
+Stimulus.prototype.toString = function() {
+	
+	if (this.event) {
+		return("id: " + this.id + ",\n RT: " + this.RT + ",\n event: " + this.event + ",\n correct: " + this.correct);
+	}
+	else {
+		return("id: " + this.id);
+	}
+};
+
