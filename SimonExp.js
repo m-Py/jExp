ed2b89148c619c084fa35644d01b294b949f21c8
m-Py <martin.papenberg@hhu.de>
2015-04-03 16:09:32 +0200

    added instruction to simon experiment

--- SimonExp.js
+++ SimonExp.js
@@ -18,11 +18,18 @@ var trials = 10;
 var cross = new Stimulus("cross", 250, 300);
 cross.addCross(30, 2);	 
 
+instrCol = rndCol();
+
 // create Stimuli - startscreen
 var startScreen = new Stimulus("startscreen", 0, 0, [32]);
-startScreen.addText("Press Space to start Simon experiment", 80, rndCol(), 0,0);
+startScreen.addText("Welcome to the experiment", 60, instrCol, 0,160);
+startScreen.addText("React to the word that is presented on the screen", 40, instrCol, 0,70);
+startScreen.addText("- Press 's' if you see 'left' -", 40, instrCol, 0,0);
+startScreen.addText("- Press 'k' if you see 'right' -", 40, instrCol, 0,-45);
+startScreen.addText(" Press 'Space' to start ", 60, instrCol, 0,-130);
 // add start screen to our simon experiment; it is the first presented stimulus then
 simon.add(startScreen);
+
 
 createBlock = function(blockRepetition) {
 	
@@ -33,6 +40,7 @@ createBlock = function(blockRepetition) {
 		startBlock.addText("Click to start " + (t+1) + ". block", 80, rndCol(), 0, 0);
 		simon.add(startBlock);	
 		
+		// add trials to each block
 		for ( var i = 0; i < trials; i++) {
 			
 			// randomly determine the trial type
@@ -53,30 +61,31 @@ createBlock = function(blockRepetition) {
 			}
 			else if (coin === 3) {
 				var id = "react_right - position_right";		
-				var correctKey = 108;
+				var correctKey = 107;
 				var text = "right";
 				var side = 300;
 				var congruency = "congruent";
 			}	
 			else if (coin === 4) {
 				var id = "react_right - position_left";		
-				var correctKey = 108;
+				var correctKey = 107;
 				var text = "right";
 				var side = -300;
 				var congruency = "incongruent";
 			}				
 			
-			// create trial:
+			// create trial: consists of fixation cross and stimulus that has randomly determined properties (see above)
 			var cross = new Stimulus("cross", 250, 300);
 			cross.addCross(30, 2);
 				
-			tempStim = new Stimulus(id, 400, rndInt(700, 1100), [115, 108], correctKey);
+			tempStim = new Stimulus(id, 400, rndInt(700, 1100), [115, 107], correctKey);
 			tempStim.addText(text, 80, rndCol(), side, 0);
 			// add some properties to the stimulus that we want so save!
 			tempStim.block = t+1; // easy way to save in which block a stimulus has been presented
 			tempStim.trial = i+1; // easy way to save the trial number of a stimulus
 			tempStim.congruency = congruency; // congruency condition
 			
+			// add fixation cross and word stimulus to the experiment
 			simon.add(cross);
 			simon.add(tempStim);
 		}
@@ -86,7 +95,7 @@ createBlock = function(blockRepetition) {
 // create experimental blocks
 createBlock(blocks);
 
-
+// add endscreen
 var end = new Stimulus("endscreen", 0, 0);
 end.addText("The experiment is over, thank you!", 80, rndCol());
 simon.add(end);
