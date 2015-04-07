8646ff255b1a41d8af656c78e496d0f91987c95c
m-Py <martin.papenberg@hhu.de>
2015-04-07 17:07:33 +0200

    added listener property to simon exp

--- SimonExp.js
+++ SimonExp.js
@@ -15,13 +15,13 @@ var trials = 10;
 
 
 // create Stimuli - fixation cross
-var cross = new Stimulus("cross", 250, 300);
+var cross = new Stimulus("cross", 250, 300, false);
 cross.addCross(30, 2);	 
 
 instrCol = rndCol();
 
 // create Stimuli - startscreen
-var startScreen = new Stimulus("startscreen", 0, 0, [32]);
+var startScreen = new Stimulus("startscreen", 0, 0, false, [32]);
 startScreen.addText("Welcome to the experiment", 60, instrCol, 0,160);
 startScreen.addText("React to the word that is presented on the screen", 40, instrCol, 0,70);
 startScreen.addText("- Press 's' if you see 'left' -", 40, instrCol, 0,0);
@@ -36,7 +36,7 @@ createBlock = function(blockRepetition) {
 	
 	for (var t = 0; t < blockRepetition; t++) {
 		
-		startBlock = new Stimulus("startblock", 0, 0);
+		startBlock = new Stimulus("startblock", 0, 0, false);
 		startBlock.addText("Click to start " + (t+1) + ". block", 80, rndCol(), 0, 0);
 		simon.add(startBlock);	
 		
@@ -78,7 +78,7 @@ createBlock = function(blockRepetition) {
 			var cross = new Stimulus("cross", 250, 300);
 			cross.addCross(30, 2);
 				
-			tempStim = new Stimulus(id, 400, rndInt(700, 1100), [115, 107], correctKey);
+			tempStim = new Stimulus(id, 400, rndInt(700, 1100), true, [115, 107], correctKey);
 			tempStim.addText(text, 80, rndCol(), side, 0);
 			// add some properties to the stimulus that we want so save!
 			tempStim.block = t+1; // easy way to save in which block a stimulus has been presented
@@ -96,7 +96,7 @@ createBlock = function(blockRepetition) {
 createBlock(blocks);
 
 // add endscreen
-var end = new Stimulus("endscreen", 0, 0);
+var end = new Stimulus("endscreen", 0, 0, false);
 end.addText("The experiment is over, thank you!", 80, rndCol());
 simon.add(end);
 
